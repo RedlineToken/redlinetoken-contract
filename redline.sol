dde886722e941026a827e3ea00fc48534504b36f
BuildTools <team@redlinetoken.com>
2021-05-05 00:47:54 +0700

    comit

--- redline.sol
+++ redline.sol
@@ -745,7 +745,7 @@ contract Redline is Context, IERC20, Ownable {
         _rOwned[_msgSender()] = _rTotal;
         
         IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F);
-         // Create a uniswap pair for this new token
+        // Create a uniswap pair for this new token
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
 
